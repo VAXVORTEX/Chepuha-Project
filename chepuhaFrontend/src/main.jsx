f17732c0c1b20a96fff777622e4f98a667cca940
Mariia Myroniuk <masamironuk76@gmail.com>
2025-12-29 15:15:15 +0200

    add chepuhaFrontend

---
+++ chepuhaFrontend/src/main.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+
+async function dataLoader(){
+  const response = await fetch('https://jsonplaceholder.typicode.com/users'); //поки тестовий json файл
+  const data = await response.json();
+  return {users: data};
+}
+
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/", 
+      element:<App />,
+      loader: dataLoader,
+    },
+  ],
+  {
+future: {
+v7_relativeSplatPath:true,
+v7_fetcherPersist:true,
+v7_normalizeFormMethod:true,
+v7_partialHydration:true,
+v7_skipActionErrorRevalidation:true,
+v7_singleFetch:true,
+},
+  },
+);
+
+createRoot(document.getElementById('root')).render(
+<StrictMode>
+  <RouterProvider router = {router}/>
+</StrictMode>,)
