18ed425e86703bf460f4db8eb985d2b695a74742
Lvovs family <lvovsfamily@Lvovss-MacBook-Pro.local>
2026-01-05 14:58:02 +0200

    Add TypeScript

--- chepuhaFrontend/src/App.tsx
+++ chepuhaFrontend/src/App.tsx
@@ -1,19 +1,29 @@
 import { useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.scss'
+
+interface User {
+  id: number;
+  name: string;
+}
+
+interface LoaderData{
+  users: User[];
+}
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
-  const {users} = useLoaderData();
+  const {users} = useLoaderData() as LoaderData;
 
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src= "/vite.svg "className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -21,16 +31,19 @@ function App() {
       </div>
       <h1>Vite + React</h1>
 
-      <h3>(Users found: {users.length})</h3> 
+      <h3>(Users found: {users?.length})</h3>
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <ul style = {{ textAlign: 'left'}}>
+        {users.map ((user) => (<li key = {user.id}> {user.name}</li>))}
+      </ul>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
