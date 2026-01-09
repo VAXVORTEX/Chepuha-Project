efcd3f0f470665a4125afca0e901aad7a0edad60
IvanSiletskyi <siletskyi.ivan@lll.kpi.ua>
2026-01-09 13:35:13 +0200

    feat - added story sheets collection type

---
+++ chepuhaBackend/src/api/story-sheet/controllers/story-sheet.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * story-sheet controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::story-sheet.story-sheet');
---
+++ chepuhaBackend/src/api/story-sheet/services/story-sheet.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * story-sheet service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::story-sheet.story-sheet');
