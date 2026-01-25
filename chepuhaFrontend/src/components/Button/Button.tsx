e99d9f595a8e14eeda536cf07023e644e45643e8
Mariia Myroniuk <masamironuk76@gmail.com>
2026-01-25 17:35:08 +0200

    feat - created RoundCard component

--- chepuhaFrontend/src/components/Button/Button.tsx
+++ chepuhaFrontend/src/components/Button/Button.tsx
@@ -8,15 +8,18 @@ interface ButtonSet{
     variant: 'primary' | 'secondary';
     phase: Phases;
     onClick: ()=> void;
+    disabled?: boolean;
 }
 
-const Button: React.FC<ButtonSet> = ({label, variant, phase, onClick }) =>{
+const Button: React.FC<ButtonSet> = ({label, variant, phase, onClick, disabled}) =>{
     const combClasses = `${styles.button} ${styles[variant]} ${styles[phase]}`;
     return (
-        <button className = {combClasses} onClick = {onClick}>
+        <button className = {combClasses} 
+        onClick = {onClick} 
+        disabled={disabled}>
             {label}
         </button>
     );
 };
-
+                                                                                                                                                                     
 export default Button;
