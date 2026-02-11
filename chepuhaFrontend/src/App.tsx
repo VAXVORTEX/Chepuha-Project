49b7eeb79a7ab9e71426f3d8e9044c1ab44eddcc
Mariia Myroniuk <masamironuk76@gmail.com>
2026-02-11 11:06:05 +0200

    feat - create main menu

--- chepuhaFrontend/src/App.tsx
+++ chepuhaFrontend/src/App.tsx
@@ -7,45 +7,67 @@ import "./App.scss";
 import { Phases } from "./types/phaseVariant";
 import Round from "./components/Round/Round";
 
+import logoImage from "./assets/images/Logo.png";
+
 function App() {
   const [phase, setPhase] = useState<Phases>(Phases.Main);
-  const [didGameStarted, setDidGameStarted] = useState(false);
+  const [didGameStart, setDidGameStart] = useState(false);
 
   const goHome = () => {
     setPhase(Phases.Main);
-    setDidGameStarted(false);
+    setDidGameStart(false);
   };
 
   const doGameStart = () => {
-    setDidGameStarted(true);
+    setDidGameStart(true);
     setPhase(Phases.Main);
+  };
+
+    const doJoinGame = () => {
+    
+  };
+
+  const doShowHistory = () => {
+    setPhase(Phases.End);
   };
 
   const doAnswerSubmit = (answer: string) => {
     setPhase(Phases.Waiting);
-
     setTimeout(() => {
       setPhase(Phases.End);
     }, 3500);
   };
 
-  return (
+return (
     <div className="app-view">
-      {!didGameStarted && phase === Phases.Main && (
+      {!didGameStart && phase === Phases.Main && (
         <>
-        <GameCode code="A7B9G6TR" className="gameCodePos" />
-        <Button
-          label="Створити гру"
-          variant="primary"
-          phase={phase}
-          onClick={doGameStart}
-        />
+          <img src={logoImage} alt="Чепуха Лого" className="logo" />
+          <div className="menu-buttons">
+            <Button
+              label="СТВОРИТИ ГРУ"
+              variant="primary"
+              phase={phase}
+              onClick={doGameStart}
+            />
+            <Button
+              label="ПРИЄДНАТИСЯ ДО ГРИ"
+              variant="primary"
+              phase={phase}
+              onClick={doJoinGame}
+            />
+            <Button
+              label="ІСТОРІЯ ІГОР"
+              variant="primary"
+              phase={phase}
+              onClick={doShowHistory}
+            />
+          </div>
         </>
       )}
 
-      {didGameStarted &&
-        (phase === Phases.Main || phase === Phases.Waiting) && (
-          <>
+      {didGameStart && (phase === Phases.Main || phase === Phases.Waiting) && (
+        <>
           <Round
             currentRound={1}
             totalRounds={8}
@@ -59,15 +81,13 @@ function App() {
             playerTotal={4}
             onSubmitAnswer={doAnswerSubmit}
           />
-          </>
-        )}
+        </>
+      )}
 
       {phase === Phases.End && (
         <Story
           title="Історія гри"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem"
+          content=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
           phase={phase}
           onHome={goHome}
           onGoBack={goHome}
@@ -76,4 +96,5 @@ Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor
     </div>
   );
 }
+
 export default App;
